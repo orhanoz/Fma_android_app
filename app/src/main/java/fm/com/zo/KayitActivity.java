0c5c0fa99706799565809aad38303fab23ad57de
orhanoz <orhanoz91@gmail.com>
2016-09-08 00:52:18 +0300

    progress dialog

--- app/src/main/java/fm/com/zo/KayitActivity.java
+++ app/src/main/java/fm/com/zo/KayitActivity.java
@@ -1,5 +1,6 @@
 package fm.com.zo;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,15 +13,15 @@ import java.util.Objects;
 
 public class KayitActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private Button kayitOl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kayit);
-
         //button declaration
-        Button kayitOl =(Button) findViewById(R.id.kayitOl);
+        kayitOl =(Button) findViewById(R.id.kayitOl);
         Button parolaHatirlama =(Button) findViewById(R.id.parolaHatirlama);
 
         // adding listeners
@@ -32,24 +33,27 @@ public class KayitActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v) {
 
-
         String email= ((EditText) findViewById(R.id.email)).getText().toString();
         String password = ((EditText) findViewById(R.id.password)).getText().toString();
         String passwordConfirmation= ((EditText) findViewById(R.id.passwordConfirmation)).getText().toString();
 
         switch (v.getId()){
-
             case R.id.kayitOl:
+                kayitOl.setEnabled(false);
+                final ProgressDialog progressDialog = new ProgressDialog(KayitActivity.this,
+                        R.style.AppTheme_Dark_Dialog);
+                progressDialog.setIndeterminate(true);
+                progressDialog.setMessage("Bilgileriniz Gönderiliyor...");
+                progressDialog.show();
 
                 if(password.equals(passwordConfirmation) && email.contains("@")){
-
                     Toast.makeText(getApplicationContext(),"Mail Gönderildi",Toast.LENGTH_LONG).show();
                     //TODO bilgiler servera gönderilir activation link mail atilir...
                     break;
-
                 }
                 else{
                     Toast.makeText(getApplicationContext(),"Hata",Toast.LENGTH_LONG).show();
+                    progressDialog.dismiss();
                     break;
                 }
 
