fcee69ab48a0461619ebeba1143524c4670a20dd
orhanoz <orhanoz91@gmail.com>
2016-09-16 22:23:40 +0300

    hedef_sicak

--- app/src/main/java/fm/com/zo/HedefSicaklikActivity.java
+++ app/src/main/java/fm/com/zo/HedefSicaklikActivity.java
@@ -31,6 +31,7 @@ public class HedefSicaklikActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
     }
